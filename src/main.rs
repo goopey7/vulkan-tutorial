4608229a7b41192f8ab4a349d6ba1f7d4cbdfd85
Sam Collier <sam@samcollier.tech>
2023-07-01 23:34:45 -0400

    create instance

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,19 @@
 	clippy::unnecessary_wraps
 )]
 
-use anyhow::Result;
 use winit::dpi::LogicalSize;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::{Window, WindowBuilder};
+
+use anyhow::{anyhow, Result};
+use log::*;
+use vulkanalia::loader::{LibloadingLoader, LIBRARY};
+use vulkanalia::window as vk_window;
+use vulkanalia::prelude::v1_0::*;
+use vulkanalia::Version;
+
+const PORTABILITY_MACOS_VERSION: Version = Version::new(1, 3, 216);
 
 fn main() -> Result<()>
 {
@@ -51,14 +59,21 @@ fn main() -> Result<()>
 
 /// Our Vulkan app.
 #[derive(Clone, Debug)]
-struct App {}
+struct App
+{
+	entry: Entry,
+	instance: Instance,
+}
 
 impl App
 {
 	/// Creates our Vulkan app.
 	unsafe fn create(window: &Window) -> Result<Self>
 	{
-		Ok(Self {})
+		let loader = LibloadingLoader::new(LIBRARY)?;
+		let entry = Entry::new(loader).map_err(|error| anyhow!(error))?;
+		let instance = create_instance(window, &entry)?;
+		Ok(Self {entry, instance})
 	}
 
 	/// Renders a frame for our Vulkan app.
@@ -70,6 +85,7 @@ impl App
 	/// Destroys our Vulkan app.
 	unsafe fn destroy(&mut self)
 	{
+		self.instance.destroy_instance(None);
 	}
 }
 
@@ -77,3 +93,39 @@ impl App
 #[derive(Clone, Debug, Default)]
 struct AppData {}
 
+unsafe fn create_instance(window: &Window, entry: &Entry) -> Result<Instance>
+{
+	let application_info = vk::ApplicationInfo::builder()
+		.application_name(b"Vulkan Tutorial (Rust)\0")
+		.application_version(vk::make_version(1, 0, 0))
+		.engine_name(b"No Engine\0")
+		.engine_version(vk::make_version(1, 0, 0))
+		.api_version(vk::make_version(1, 0, 0));
+
+	let mut extensions = vk_window::get_required_instance_extensions(window)
+		.iter()
+		.map(|extension| extension.as_ptr())
+		.collect::<Vec<_>>();
+
+	// Since vulkan on macOS doesn't conform to spec
+	// we gotta enable some additional extensions
+	// if the vulkan sdk version is 1.3.216 or higher
+	let flags = if cfg!(target_os = "macos") && entry.version()? >= PORTABILITY_MACOS_VERSION
+				{
+					info!("Enabling macOS portability extensions");
+					extensions.push(vk::KHR_GET_PHYSICAL_DEVICE_PROPERTIES2_EXTENSION.name.as_ptr());
+					extensions.push(vk::KHR_PORTABILITY_ENUMERATION_EXTENSION.name.as_ptr());
+					vk::InstanceCreateFlags::ENUMERATE_PORTABILITY_KHR
+				}
+				else
+				{
+					vk::InstanceCreateFlags::empty()
+				};
+
+	let info = vk::InstanceCreateInfo::builder()
+		.application_info(&application_info)
+		.enabled_extension_names(&extensions)
+		.flags(flags);
+
+	Ok(entry.create_instance(&info, None)?)
+}
