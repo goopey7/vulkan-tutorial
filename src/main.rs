42276623d015a1b21da3e973d37e3b4e3db7a8f4
Sam Collier <sam@samcollier.tech>
2023-07-02 16:14:30 -0400

    validation layers

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,17 @@ use vulkanalia::window as vk_window;
 use vulkanalia::prelude::v1_0::*;
 use vulkanalia::Version;
 
+use std::collections::HashSet;
+use std::ffi::CStr;
+use std::os::raw::c_void;
+
+use vulkanalia::vk::{ExtDebugUtilsExtension, DebugUtilsMessageTypeFlagsEXT, DebugUtilsMessageSeverityFlagsEXT};
+
+
 const PORTABILITY_MACOS_VERSION: Version = Version::new(1, 3, 216);
+const VALIDATION_ENABLED: bool = cfg!(debug_assertions);
+const VALIDATION_LAYER: vk::ExtensionName =
+	vk::ExtensionName::from_bytes(b"VK_LAYER_KHRONOS_validation");
 
 fn main() -> Result<()>
 {
@@ -63,6 +73,7 @@ struct App
 {
 	entry: Entry,
 	instance: Instance,
+	data: AppData,
 }
 
 impl App
@@ -72,8 +83,9 @@ impl App
 	{
 		let loader = LibloadingLoader::new(LIBRARY)?;
 		let entry = Entry::new(loader).map_err(|error| anyhow!(error))?;
-		let instance = create_instance(window, &entry)?;
-		Ok(Self {entry, instance})
+		let mut data = AppData::default();
+		let instance = create_instance(window, &entry, &mut data)?;
+		Ok(Self {entry, instance, data})
 	}
 
 	/// Renders a frame for our Vulkan app.
@@ -85,15 +97,22 @@ impl App
 	/// Destroys our Vulkan app.
 	unsafe fn destroy(&mut self)
 	{
+		if VALIDATION_ENABLED
+		{
+			self.instance.destroy_debug_utils_messenger_ext(self.data.messenger, None);
+		}
 		self.instance.destroy_instance(None);
 	}
 }
 
 /// The Vulkan handles and associated properties used by our Vulkan app.
 #[derive(Clone, Debug, Default)]
-struct AppData {}
+struct AppData
+{
+	messenger: vk::DebugUtilsMessengerEXT,
+}
 
-unsafe fn create_instance(window: &Window, entry: &Entry) -> Result<Instance>
+unsafe fn create_instance(window: &Window, entry: &Entry, data: &mut AppData) -> Result<Instance>
 {
 	let application_info = vk::ApplicationInfo::builder()
 		.application_name(b"Vulkan Tutorial (Rust)\0")
@@ -102,10 +121,34 @@ unsafe fn create_instance(window: &Window, entry: &Entry) -> Result<Instance>
 		.engine_version(vk::make_version(1, 0, 0))
 		.api_version(vk::make_version(1, 0, 0));
 
+	let available_layers = entry.enumerate_instance_layer_properties()?
+		.iter()
+		.map(|layer| layer.layer_name)
+		.collect::<HashSet<_>>();
+
+	if VALIDATION_ENABLED && !available_layers.contains(&VALIDATION_LAYER)
+	{
+		return Err(anyhow!("Validation layer requested but not supported"));
+	}
+
+	let layers = if VALIDATION_ENABLED
+	{
+		vec![VALIDATION_LAYER.as_ptr()]
+	}
+	else
+	{
+		vec![]
+	};
+
 	let mut extensions = vk_window::get_required_instance_extensions(window)
 		.iter()
 		.map(|extension| extension.as_ptr())
 		.collect::<Vec<_>>();
+
+	if VALIDATION_ENABLED
+	{
+		extensions.push(vk::EXT_DEBUG_UTILS_EXTENSION.name.as_ptr());
+	}
 
 	// Since vulkan on macOS doesn't conform to spec
 	// we gotta enable some additional extensions
@@ -122,10 +165,63 @@ unsafe fn create_instance(window: &Window, entry: &Entry) -> Result<Instance>
 					vk::InstanceCreateFlags::empty()
 				};
 
-	let info = vk::InstanceCreateInfo::builder()
+	let mut info = vk::InstanceCreateInfo::builder()
 		.application_info(&application_info)
 		.enabled_extension_names(&extensions)
+		.enabled_layer_names(&layers)
 		.flags(flags);
 
-	Ok(entry.create_instance(&info, None)?)
+	let mut debug_info = vk::DebugUtilsMessengerCreateInfoEXT::builder()
+		.message_severity(DebugUtilsMessageSeverityFlagsEXT::all())
+		.message_type(DebugUtilsMessageTypeFlagsEXT::all())
+		.user_callback(Some(debug_callback));
+
+	if VALIDATION_ENABLED
+	{
+		info = info.push_next(&mut debug_info);
+	}
+
+	let instance = entry.create_instance(&info, None)?;
+
+	if VALIDATION_ENABLED
+	{
+		let debug_info = vk::DebugUtilsMessengerCreateInfoEXT::builder()
+			.message_severity(vk::DebugUtilsMessageSeverityFlagsEXT::all())
+			.message_type(vk::DebugUtilsMessageTypeFlagsEXT::all())
+			.user_callback(Some(debug_callback));
+
+		data.messenger = instance.create_debug_utils_messenger_ext(&debug_info, None)?;
+	}
+
+	Ok(instance)
+}
+
+extern "system" fn debug_callback(
+	severity: vk::DebugUtilsMessageSeverityFlagsEXT,
+	type_: vk::DebugUtilsMessageTypeFlagsEXT,
+	data: *const vk::DebugUtilsMessengerCallbackDataEXT,
+	_: *mut c_void,
+	) -> vk::Bool32
+{
+	let data = unsafe { *data };
+	let message = unsafe { CStr::from_ptr(data.message) }.to_string_lossy();
+
+	if severity >= vk::DebugUtilsMessageSeverityFlagsEXT::ERROR
+	{
+		error!("({:?}) {}", type_, message);
+	}
+	else if severity >= vk::DebugUtilsMessageSeverityFlagsEXT::WARNING
+	{
+		warn!("({:?}) {}", type_, message);
+	}
+	else if severity >= vk::DebugUtilsMessageSeverityFlagsEXT::INFO
+	{
+		info!("({:?}) {}", type_, message);
+	}
+	else
+	{
+		trace!("({:?}) {}", type_, message);
+	}
+
+	vk::FALSE
 }
