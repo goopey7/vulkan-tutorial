cf30f8fe8f72b085cb7bb3f5ddc46b07c36edd72
Sam Collier <sam@samcollier.tech>
2023-07-02 18:19:59 -0400

    logical device and queues

--- src/main.rs
+++ src/main.rs
@@ -76,6 +76,7 @@ struct App
 	entry: Entry,
 	instance: Instance,
 	data: AppData,
+	device: Device,
 }
 
 impl App
@@ -88,7 +89,8 @@ impl App
 		let mut data = AppData::default();
 		let instance = create_instance(window, &entry, &mut data)?;
 		select_physical_device(&instance, &mut data);
-		Ok(Self {entry, instance, data})
+		let device = create_logical_device(&entry, &instance, &mut data)?;
+		Ok(Self {entry, instance, data, device})
 	}
 
 	/// Renders a frame for our Vulkan app.
@@ -100,6 +102,7 @@ impl App
 	/// Destroys our Vulkan app.
 	unsafe fn destroy(&mut self)
 	{
+		self.device.destroy_device(None);
 		if VALIDATION_ENABLED
 		{
 			self.instance.destroy_debug_utils_messenger_ext(self.data.messenger, None);
@@ -114,6 +117,7 @@ struct AppData
 {
 	messenger: vk::DebugUtilsMessengerEXT,
 	physical_device: vk::PhysicalDevice,	
+	graphics_queue: vk::Queue,
 }
 
 unsafe fn create_instance(window: &Window, entry: &Entry, data: &mut AppData) -> Result<Instance>
@@ -266,6 +270,50 @@ unsafe fn select_physical_device(instance: &Instance, data: &mut AppData) -> Res
 	}
 
 	Err(anyhow!("No suitable physical device found"))
+}
+
+unsafe fn create_logical_device(
+	entry: &Entry,
+	instance: &Instance,
+	data: &mut AppData,
+	) -> Result<Device>
+{
+	let indices = QueueFamilyIndices::get(instance, data, data.physical_device)?;
+	let queue_priorities = &[1.0];
+
+	let queue_info = vk::DeviceQueueCreateInfo::builder()
+		.queue_family_index(indices.graphics)
+		.queue_priorities(queue_priorities);
+
+	let layers = if VALIDATION_ENABLED
+	{
+		vec![VALIDATION_LAYER.as_ptr()]
+	}
+	else
+	{
+		vec![]
+	};
+
+	let mut extensions = vec![];
+
+	// Since vulkan on macOS doesn't conform to spec
+	if cfg!(target_os = "macos") && entry.version()? >= PORTABILITY_MACOS_VERSION
+	{
+		extensions.push(vk::KHR_PORTABILITY_SUBSET_EXTENSION.name.as_ptr());
+	}
+
+	let features = vk::PhysicalDeviceFeatures::builder();
+
+	let queue_infos = &[queue_info];
+	let info = vk::DeviceCreateInfo::builder()
+		.queue_create_infos(queue_infos)
+		.enabled_extension_names(&extensions)
+		.enabled_layer_names(&layers)
+		.enabled_features(&features);
+
+	let device = instance.create_device(data.physical_device, &info, None)?;
+	data.graphics_queue = device.get_device_queue(indices.graphics, 0);
+	Ok(device)
 }
 
 extern "system" fn debug_callback(
