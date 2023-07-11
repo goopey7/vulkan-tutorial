563fafbc23c66a4b99ec794ef141ef40fa06b5e6
Sam Collier <sam@samcollier.tech>
2023-07-10 23:24:00 -0400

    descriptor pool and sets

--- src/main.rs
+++ src/main.rs
@@ -145,6 +145,8 @@ impl App
 		create_vertex_buffer(&instance, &device, &mut data)?;
 		create_index_buffer(&instance, &device, &mut data)?;
 		create_uniform_buffers(&instance, &device, &mut data)?;
+		create_descriptor_pool(&device, &mut data)?;
+		create_descriptor_sets(&device, &mut data)?;
 		create_command_buffers(&device, &mut data)?;
 		create_sync_objects(&device, &mut data)?;
 		Ok(Self {entry, instance, data, device, frame: 0, resized: false, start: Instant::now(),})
@@ -276,6 +278,8 @@ impl App
 		create_pipeline(&self.device, &mut self.data)?;
 		create_framebuffers(&self.device, &mut self.data)?;
 		create_uniform_buffers(&self.instance, &self.device, &mut self.data)?;
+		create_descriptor_pool(&self.device, &mut self.data)?;
+		create_descriptor_sets(&self.device, &mut self.data)?;
 		create_command_buffers(&self.device, &mut self.data)?;
 		self.data
 			.images_in_flight
@@ -285,6 +289,7 @@ impl App
 
 	unsafe fn destroy_swapchain(&mut self)
 	{
+		self.device.destroy_descriptor_pool(self.data.descriptor_pool, None);
 		self.data.uniform_buffers
 			.iter()
 			.for_each(|ub| self.device.destroy_buffer(*ub, None));
@@ -376,6 +381,8 @@ struct AppData
 	index_buffer_memory: vk::DeviceMemory,
 	uniform_buffers: Vec<vk::Buffer>,
 	uniform_buffers_memory: Vec<vk::DeviceMemory>,
+	descriptor_pool: vk::DescriptorPool,
+	descriptor_sets: Vec<vk::DescriptorSet>,
 }
 
 unsafe fn create_instance(window: &Window, entry: &Entry, data: &mut AppData) -> Result<Instance>
@@ -945,7 +952,7 @@ unsafe fn create_pipeline(
 		.polygon_mode(vk::PolygonMode::FILL)
 		.line_width(1.0)
 		.cull_mode(vk::CullModeFlags::BACK)
-		.front_face(vk::FrontFace::CLOCKWISE)
+		.front_face(vk::FrontFace::COUNTER_CLOCKWISE)
 		.depth_bias_enable(false);
 
 	let multisample_state = vk::PipelineMultisampleStateCreateInfo::builder()
@@ -1094,6 +1101,13 @@ unsafe fn create_command_buffers(
 		device.cmd_bind_pipeline(*command_buffer, vk::PipelineBindPoint::GRAPHICS, data.pipeline);
 		device.cmd_bind_vertex_buffers(*command_buffer, 0, &[data.vertex_buffer], &[0]);
 		device.cmd_bind_index_buffer(*command_buffer, data.index_buffer, 0, vk::IndexType::UINT16);
+		device.cmd_bind_descriptor_sets(
+			*command_buffer,
+			vk::PipelineBindPoint::GRAPHICS,
+			data.pipeline_layout,
+			0,
+			&[data.descriptor_sets[i]],
+			&[]);
 		device.cmd_draw_indexed(*command_buffer, INDICES.len() as u32, 1, 0, 0, 0);
 		device.cmd_end_render_pass(*command_buffer);
 		device.end_command_buffer(*command_buffer)?;
@@ -1434,6 +1448,56 @@ unsafe fn create_descriptor_set_layout(
 
 	data.descriptor_set_layout = device.create_descriptor_set_layout(&info, None)?;
 
+	Ok(())
+}
+
+unsafe fn create_descriptor_pool(
+	device: &Device,
+	data: &mut AppData
+	) -> Result<()>
+{
+	let ubo_size = vk::DescriptorPoolSize::builder()
+		.type_(vk::DescriptorType::UNIFORM_BUFFER)
+		.descriptor_count(data.swapchain_images.len() as u32);
+
+	let pool_sizes = &[ubo_size];
+	let info = vk::DescriptorPoolCreateInfo::builder()
+		.pool_sizes(pool_sizes)
+		.max_sets(data.swapchain_images.len() as u32);
+
+	data.descriptor_pool = device.create_descriptor_pool(&info, None)?;
+	Ok(())
+}
+
+unsafe fn create_descriptor_sets(
+	device: &Device,
+	data: &mut AppData,
+	) -> Result<()>
+{
+	let layouts = vec![data.descriptor_set_layout; data.swapchain_images.len()];
+	let info = vk::DescriptorSetAllocateInfo::builder()
+		.descriptor_pool(data.descriptor_pool)
+		.set_layouts(&layouts);
+
+	data.descriptor_sets = device.allocate_descriptor_sets(&info)?;
+
+	for i in 0..data.swapchain_images.len()
+	{
+		let info = vk::DescriptorBufferInfo::builder()
+			.buffer(data.uniform_buffers[i])
+			.offset(0)
+			.range(size_of::<UniformBufferObject>() as u64);
+
+		let buffer_info = &[info];
+		let ubo_write = vk::WriteDescriptorSet::builder()
+			.dst_set(data.descriptor_sets[i])
+			.dst_binding(0)
+			.dst_array_element(0)
+			.descriptor_type(vk::DescriptorType::UNIFORM_BUFFER)
+			.buffer_info(buffer_info);
+
+		device.update_descriptor_sets(&[ubo_write], &[] as &[vk::CopyDescriptorSet]);
+	}
 	Ok(())
 }
 
